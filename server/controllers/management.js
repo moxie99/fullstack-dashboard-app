f65264603f00efa9ad6f2e4cc69d4e32ba5eeeb6
Adeolu David <adeolusegun99@gmail.com>
2023-03-12 02:22:28 +0100

    renamed file

--- server/controllers/management.js
+++ server/controllers/management.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose"
-import User from "../models/User.js"
+import User from "../models/user.js"
 import Transaction from "../models/Transaction.js"
 
 export const getAdmins = async (req, res) => {
